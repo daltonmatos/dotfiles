5d2e49621cb4bcd7116af6a83ac46e723467df17
Dalton Barreto <daltonmatos@gmail.com>
2021-03-17 18:58:18 -0300

    Mudando o script de BT status pra receber o nome do device

---
+++ i3/polybar/scripts/jbl-bt-status.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Device Class:
+# JBL GO: 0x00200414
+# JBL TUNEBT500: 0x00240404
+
+DEVICE_NAME=${1:-}
+
+bluetooth_print() {
+        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+            devices_paired=$(bluetoothctl paired-devices | grep "${DEVICE_NAME}" | grep Device | cut -d ' ' -f 2)
+            device_info=$(bluetoothctl info "${devices_paired}")
+
+            bat_level=$(/home/daltonmatos/.pyenv/shims/bluetooth_battery "${devices_paired}" | cut -d ' ' -f 6)
+            if echo "$device_info" | grep -q "Connected: yes"; then
+                device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+                printf " %s" "${bat_level}"
+              else
+                echo "ﳌ"
+            fi
+        fi
+}
+
+bluetooth_print
