9455cb353752a2a12b19bf658436681abafed83d
Dalton Barreto <daltonmatos@gmail.com>
2021-03-21 17:16:20 -0300

    Comentando a leitura do nivel de bateria do BT

--- i3/polybar/scripts/jbl-bt-status.sh
+++ i3/polybar/scripts/jbl-bt-status.sh
@@ -11,8 +11,8 @@ bluetooth_print() {
             devices_paired=$(bluetoothctl paired-devices | grep "${DEVICE_NAME}" | grep Device | cut -d ' ' -f 2)
             device_info=$(bluetoothctl info "${devices_paired}")
 
-            bat_level=$(/home/daltonmatos/.pyenv/shims/bluetooth_battery "${devices_paired}" | cut -d ' ' -f 6)
             if echo "$device_info" | grep -q "Connected: yes"; then
+                #bat_level=$(/home/daltonmatos/.pyenv/shims/bluetooth_battery "${devices_paired}" | cut -d ' ' -f 6)
                 device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
                 printf " %s" "${bat_level}"
               else
