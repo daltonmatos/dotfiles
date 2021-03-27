205324e81cb57dc40bb07ebd1e61fb121410ac2e
Dalton Barreto <daltonmatos@gmail.com>
2021-03-27 18:19:56 -0300

    Migra spacex-next-launch para API v4

--- i3/polybar/scripts/spacex-next-launch.sh
+++ i3/polybar/scripts/spacex-next-launch.sh
@@ -4,12 +4,12 @@
 spacex_launch=$(curl -sf https://api.spacexdata.com/v4/launches/next)
 
 if [ -n "$spacex_launch" ]; then
-    spacex_precision=$(echo "$spacex_launch" | jq -r '.tentative_max_precision' )
-    spacex_timestamp=$(echo "$spacex_launch" | jq -r '.launch_date_unix' )
-    spacex_mission_name=$(echo "$spacex_launch" | jq -r '.mission_name' )
+    spacex_precision=$(echo "$spacex_launch" | jq -r '.date_precision' )
+    spacex_timestamp=$(echo "$spacex_launch" | jq -r '.date_unix' )
+    spacex_mission_name=$(echo "$spacex_launch" | jq -r '.name' )
     spacex_duration=$((spacex_timestamp - $(date +%s)))
 
-    if [ "$spacex_precision" = "hour" ] && [ "$spacex_duration" -lt 43200 ]; then
+    if [ "$spacex_precision" = "hour" ] && [ "$spacex_duration" -lt 43200 ] && [ "${spacex_duration}" -gt 0 ]; then
         echo " ${spacex_mission_name} $(date +"%Hh%Mm" -u --date @$spacex_duration)"
     else
         echo ""
