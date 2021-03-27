9186fd00ec3594ed43687ea9fdedd47c3dedb67a
Dalton Barreto <daltonmatos@gmail.com>
2021-03-27 18:33:00 -0300

    Removendo indicação de bateria do BT

--- i3/polybar/scripts/jbl-bt-status.sh
+++ i3/polybar/scripts/jbl-bt-status.sh
@@ -14,7 +14,8 @@ bluetooth_print() {
             if echo "$device_info" | grep -q "Connected: yes"; then
                 #bat_level=$(/home/daltonmatos/.pyenv/shims/bluetooth_battery "${devices_paired}" | cut -d ' ' -f 6)
                 device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
-                printf " %s" "${bat_level}"
+                #printf " %s" "${bat_level}"
+                printf ""
               else
                 echo "ﳌ"
             fi
