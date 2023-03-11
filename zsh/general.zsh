bc0ec022f3c3c3288082ea373873b89bc5bb3c23
Dalton Barreto <daltonmatos@gmail.com>
2023-03-11 15:08:08 -0300

    Movendo configsd e PATH para um arquivo separado

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -1,9 +1,6 @@
 
 export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
 export LANG=en_US.UTF-8
-# Customize to your needs...
-export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/i686-pc-linux-gnu/gcc-bin/4.5.3:/usr/i686-pc-linux-gnu/x86_64-pc-linux-gnu/gcc-bin/4.5.3:/usr/games/bin:/home/daltonmatos/src/less.js/bin/:~/.local/bin
-
 
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
