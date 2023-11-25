3faed1ef4148b6c19954250227319fca69915f25
Dalton Barreto <daltonmatos@gmail.com>
2023-11-25 17:58:20 -0300

    Trocando powerlevel9k por starship prompt
    refs: https://starship.rs/

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,13 +1,8 @@
-
-# Antigen init
 source $(dirname ${ZDOTDIR}/.zshrc)/antigen/antigen.zsh
-# Load the oh-my-zsh's library.
 antigen use oh-my-zsh
-# Load the theme.
-#antigen theme candy
-antigen theme bhilburn/powerlevel9k powerlevel9k
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
+#antigen theme bhilburn/powerlevel9k powerlevel9k
+
 antigen bundle sudo
 antigen bundle git
 antigen bundle heroku
@@ -27,5 +22,4 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions src
 
-# Tell antigen that you're done.
 antigen apply
