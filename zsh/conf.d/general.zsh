be49ae8c0d88ccb8501aed9ada476066c209e142
Dalton Barreto <daltonmatos@gmail.com>
2024-01-01 14:15:41 -0300

    Enxugando general.zsh

--- zsh/conf.d/general.zsh
+++ zsh/conf.d/general.zsh
@@ -1,21 +1,5 @@
 
-export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
-#export LANG=en_US.UTF-8
-export LC_CTYPE=pt_BR.UTF-8
-
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_SPACE
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
 setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
-setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
-
-autoload -U compinit promptinit
-compinit
-export TERMINAL="xfce4-terminal"
 
 zstyle ':completion::complete:*' use-cache 1
