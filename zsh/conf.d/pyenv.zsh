03d9a6a9abeba9df0ca6b13ee2fcbbaa52626bd9
Dalton Barreto <daltonmatos@gmail.com>
2024-01-06 23:00:52 -0300

    Movendo ativação do pyenv para conf.d/

--- zsh/conf.d/pyenv.zsh
+++ zsh/conf.d/pyenv.zsh
@@ -1,4 +1,20 @@
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
+export PYENV_ROOT=$HOME/.pyenv
+
+STATIC_FILE=${HOME}/.pyenv.static.zsh
+PYENV_BIN=${PYENV_ROOT}/bin/pyenv
+
+
+if [[ ! -d ${PYENV_ROOT} ]];then
+  echo "Installing pyenv..."
+  git clone https://github.com/pyenv/pyenv .pyenv
+  git clone https://github.com/pyenv/pyenv-update .pyenv/plugins/pyenv-update
 fi
+
+if [[ ! -f ${STATIC_FILE} ]];then
+  touch ${STATIC_FILE}
+  ${PYENV_BIN} init --path >> ${STATIC_FILE}
+  ${PYENV_BIN} init - >> ${STATIC_FILE}
+fi
+
+export PATH=${PYENV_ROOT}/bin:${PATH}
+source ${STATIC_FILE}
