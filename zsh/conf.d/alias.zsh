c9619447e5f332ea8dc24c78e1cdf69047e10339
Dalton Barreto <daltonmatos@gmail.com>
2024-10-18 14:55:39 -0300

    update randpwd alias

--- zsh/conf.d/alias.zsh
+++ zsh/conf.d/alias.zsh
@@ -1,4 +1,4 @@
 alias t='terraform'
 alias tm='terramate'
-alias randpwd='dd if=/dev/random bs=1 count=64 2>/dev/null | tr -dc "[:alpha:]"'
+alias randpwd='dd if=/dev/random bs=1 count=128 2>/dev/null | tr -dc "[:alpha:]"'
 alias myip='curl https://ipinfo.io/ip'
