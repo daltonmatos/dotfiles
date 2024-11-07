2272e0ab555da71d651e264ed824a9a0783c7e32
Dalton Barreto <daltonmatos@gmail.com>
2024-11-07 20:12:57 -0300

    Adiciona alias para neovim

--- zsh/conf.d/alias.zsh
+++ zsh/conf.d/alias.zsh
@@ -2,3 +2,5 @@ alias t='terraform'
 alias tm='terramate'
 alias randpwd='dd if=/dev/random bs=1 count=128 2>/dev/null | tr -dc "[:alpha:]"'
 alias myip='curl https://ipinfo.io/ip'
+alias vim=nvim
+alias v=vim
